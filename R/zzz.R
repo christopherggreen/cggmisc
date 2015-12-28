5ad0beef881e91f03df1d12b137284af989a6c89
Christopher G. Green <christopher.g.green@gmail.com>
2015-12-27 22:27:06 -0800

    Porting more of the stable distribution functions over to cggmisc. Need to move location.m.

--- R/zzz.R
+++ R/zzz.R
@@ -1,1 +1,12 @@
-which.na <- function(x) which(is.na(x))
+# various utility functions
+
+# previously we imported the splus2R package
+# for these, but that creates lots of conflicts.
+# they are simple enough to define internally
+
+anyMissing <- function(x) any(is.na(unlist(x)))
+which.na   <- function(x) which(is.na(x))
+
+# for historical compatibility with other 
+# scripts
+memsize <- memory.size
